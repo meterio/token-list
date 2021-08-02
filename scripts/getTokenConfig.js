5780bfef3ca3641196853418db72e317f9ceb0aa
yanfeizuo <18856856693@163.com>
2021-08-02 23:42:02 +0800

    modify token_mapping/TOKEN.json files;update parse TOKEN.json ways;

--- scripts/getTokenConfig.js
+++ scripts/getTokenConfig.js
@@ -5,34 +5,15 @@ const HOST_URL =
   "https://raw.githubusercontent.com/meterio/bridge-tokens/master";
 const LOGOS_DIR = "data/resource-logos";
 
-const ETH_TITLE = {
-  address: "Ethereum Token Address",
-  name: "Ethereum Token Name",
-  symbol: "Ethereum Token Symbol",
+const TITLE = {
   resourceId: "Resource ID",
-  nativeDecimals: "Ethereum Token Decimals",
-};
-
-const METER_TITLE = {
-  address: "Meter Token Address",
-  name: "Meter Token Name",
-  symbol: "Meter Token Symbol",
-  resourceId: "Resource ID",
-};
-
-const BSC_TITLE = {
-  address: "BSC Token Address",
-  name: "BSC Token Name",
-  symbol: "BSC Token Symbol",
-  resourceId: "Resource ID",
-  tokenProxy: "BSC Token Proxy",
+  address: "Token Address",
+  name: "Token Name",
+  symbol: "Token Symbol",
 };
 
 /**
  * copy logo from resource-logos to destDir
- * @param {array} resourceLogoDirs
- * @param {string} resourceId
- * @param {string} destDir
  */
 const copy = (resourceLogoDirs, resourceId, destDir) => {
   if (resourceLogoDirs.includes(resourceId)) {
@@ -73,41 +54,39 @@ const copy = (resourceLogoDirs, resourceId, destDir) => {
 };
 /**
  * copy token logo
- * @param {obj[]} tokenMappings
- * @param {[]} resourceLogoDirs
  */
 const copyTokenLogo = (tokenMappings, resourceLogoDirs) => {
   tokenMappings.forEach((item) => {
-    const resource_id = item["Resource ID"];
+    const e = item.Ethereum;
+    const m = item.Meter;
+    const b = item.BSC;
+    const resourceId = item[TITLE.resourceId];
 
-    if (item[ETH_TITLE.address] !== "") {
-      const eth_dist_dir =
-        "tokens/eth/" + item[ETH_TITLE.address].toLowerCase();
-      copy(resourceLogoDirs, resource_id, eth_dist_dir);
+    if (e) {
+      const eth_dist_dir = "tokens/eth/" + e[TITLE.address].toLowerCase();
+      copy(resourceLogoDirs, resourceId, eth_dist_dir);
     }
 
-    if (item[METER_TITLE.address] !== "") {
-      const meter_dist_dir =
-        "tokens/meter/" + item[METER_TITLE.address].toLowerCase();
-      copy(resourceLogoDirs, resource_id, meter_dist_dir);
+    if (m) {
+      const meter_dist_dir = "tokens/meter/" + m[TITLE.address].toLowerCase();
+      copy(resourceLogoDirs, resourceId, meter_dist_dir);
     }
 
-    if (item[BSC_TITLE.address] !== "") {
-      const bsc_dist_dir =
-        "tokens/bsc/" + item[BSC_TITLE.address].toLowerCase();
-      copy(resourceLogoDirs, resource_id, bsc_dist_dir);
+    if (b) {
+      const bsc_dist_dir = "tokens/bsc/" + b[TITLE.address].toLowerCase();
+      copy(resourceLogoDirs, resourceId, bsc_dist_dir);
     }
   });
 };
 
 const loadTokenMappings = () => {
   const pre = path.join(__dirname, "..", "data/token_mappings");
-  const tokenListPath = path.join(pre, "token-list.json");
-  const tokenList = JSON.parse(fs.readFileSync(tokenListPath));
+
+  const tokenList = fs.readdirSync(pre);
 
   const mappings = [];
   for (const token of tokenList) {
-    const tokenPath = path.join(pre, `${token}.json`);
+    const tokenPath = path.join(pre, `${token}`);
     const data = JSON.parse(fs.readFileSync(tokenPath));
     mappings.push(data);
   }
@@ -115,24 +94,61 @@ const loadTokenMappings = () => {
   return mappings;
 };
 
+const getImageUri = (resourceId) => {
+  return `${HOST_URL}/${LOGOS_DIR}/${resourceId}/logo.png`;
+};
+
+const getObj = (data, resourceId) => {
+  return {
+    address: data[TITLE.address],
+    name: data[TITLE.name],
+    symbol: data[TITLE.symbol],
+    imageUri: getImageUri(resourceId),
+    native: false,
+    resourceId: resourceId,
+  };
+};
+
+const parseTokenMappings = (tokenMappings) => {
+  const eth = [];
+  const meter = [];
+  const bsc = [];
+  tokenMappings.forEach((item) => {
+    const e = item.Ethereum;
+    const m = item.Meter;
+    const b = item.BSC;
+    const resourceId = item[TITLE.resourceId];
+    if (e) {
+      const obj = getObj(e, resourceId);
+      if (obj.symbol === "ETH") {
+        obj.native = true;
+        obj.nativeDecimals = e["Token Decimals"];
+      }
+      eth.push(obj);
+    }
+    if (m) {
+      const obj = getObj(m, resourceId);
+      meter.push(obj);
+    }
+    if (b) {
+      const obj = getObj(b, resourceId);
+      if (obj.symbol === "UTU") {
+        obj.native = true;
+        obj.tokenProxy = b["Token Proxy"];
+      }
+      bsc.push(obj);
+    }
+  });
+
+  return { eth, meter, bsc };
+};
+
 function main() {
   const token_mappings = loadTokenMappings();
 
-  const eth = generate(token_mappings, ETH_TITLE);
-  const meter = generate(token_mappings, METER_TITLE);
-  const bsc = generate(token_mappings, BSC_TITLE);
+  // console.log(token_mappings);
 
-  const tokens = {
-    ETH: eth.data,
-    MTR: meter.data,
-    BNB: bsc.data,
-  };
-
-  fs.writeFileSync(
-    path.join(__dirname, "..", "tokens.json"),
-    JSON.stringify(tokens, null, 2)
-  );
-  console.log("generate tokens.json success.");
+  const { eth, meter, bsc } = parseTokenMappings(token_mappings);
 
   fs.writeFileSync(
     path.join(__dirname, "..", "eth.json"),
@@ -161,44 +177,3 @@ function main() {
 }
 
 main();
-
-/**
- * generate json by mapping and title
- * @param {obj[]} mappings
- * @param {obj} title
- * @returns obj
- */
-function generate(mappings, title) {
-  let tokens = { data: [] };
-  mappings.forEach((item) => {
-    let o = {
-      address: item[title.address],
-      name: item[title.name],
-      symbol: item[title.symbol],
-      imageUri:
-        HOST_URL +
-        "/" +
-        LOGOS_DIR +
-        "/" +
-        item[title.resourceId] +
-        "/" +
-        "logo.png",
-      native: false,
-      resourceId: item[title.resourceId],
-    };
-    if (!o.address && !o.name && !o.symbol) {
-      // no name, address and symbol, skip
-      return;
-    }
-    if (o.symbol === "ETH") {
-      o.native = true;
-      o.nativeDecimals = item[title.nativeDecimals];
-    }
-    if (o.symbol === "UTU") {
-      o.tokenProxy = item[title.tokenProxy];
-    }
-    tokens.data.push(o);
-  });
-
-  return tokens;
-}
