7e48ef33fef103159193c7cf9d51c22d62d12c83
Simon Zhang <zhanghan.simon@gmail.com>
2021-08-19 08:41:38 +0800

    change schema to enable . in token symbol

--- scripts/utils/schema.js
+++ scripts/utils/schema.js
@@ -22,7 +22,7 @@ const tokenSchema = {
 
     // chain-specific configs, optional
     name: { type: 'string', pattern: '^[0-9a-zA-Z._ ]{1,100}$' }, // string of 1-100 digit/letter
-    symbol: { type: 'string', pattern: '^[0-9a-zA-Z]{1,9}$' }, // string of 1-9 digit/upper_letter
+    symbol: { type: 'string', pattern: '^[0-9a-zA-Z.]{1,9}$' }, // string of 1-9 digit/upper_letter
     decimals: { type: 'number', maximum: 20, minimum: 1 }, // number between 1-20
     native: { type: 'boolean' }, // true - native | false - ERC20
     tokenProxy: { type: 'string' }, // optional
@@ -36,7 +36,7 @@ const schema = {
     resourceID: { type: 'string', pattern: '^0x[0-9a-z]{64}$' }, // string of 0x + 64 digit/lower_letter
     testResourceID: { type: 'string', pattern: '^0x[0-9a-zA-Z]{64}$' }, // string of 0x + 64 digit/letter
     name: { type: 'string', pattern: '^[0-9a-zA-Z._ ]{1,100}$' }, // string of 1-100 digit/letter
-    symbol: { type: 'string', pattern: '^[0-9a-zA-Z]{1,9}$' }, // string of 1-9 digit/upper_letter
+    symbol: { type: 'string', pattern: '^[0-9a-zA-Z.]{1,9}$' }, // string of 1-9 digit/upper_letter
     decimals: { type: 'number', maximum: 20, minimum: 1 }, // number between 1-20
     enable: { type: 'boolean' }, // true - enable | false - disable
     tokens: { type: 'array', items: tokenSchema, minItems: 1 },
